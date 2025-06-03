a9a17dbed93d3e718b356fa60e9d607c4020e42d
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-03 23:45:45 +0100

    Added a path to dynamically fetch each transaction based on its primary key (id)

--- finance_tracker/api/urls.py
+++ finance_tracker/api/urls.py
@@ -3,5 +3,6 @@ from .views import homeAPI, TransactionList
 
 urlpatterns = [
     path('', homeAPI),
-    path('transactions/', TransactionList.as_view(), name='transaction-list')
+    path('transactions/', TransactionList.as_view(), name='transaction-list'),
+    path("transactions/<int:pk>", TransactionDetail.as_view(), name="transaction-detail"),
 ]
