c7a15cb4e737abb7719319b8a3ce10a100264fc2
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-03 23:23:40 +0100

    Created the transactions list class-based view with a get function

--- finance_tracker/api/views.py
+++ finance_tracker/api/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Transaction
+from .serializers import TransactionSerializer
 from django.http import HttpResponse
 
 # Create your views here.
 def homeAPI(request):
     return HttpResponse("Home of the API")
+
+
+class TransactionList(APIView):
+    def get(self, request):
+        transactions = Transaction.objects.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
+    
+    
