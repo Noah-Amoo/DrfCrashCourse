38e66a95fc935cd2c23c3f64f400a5660861893f
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-03 23:30:33 +0100

    Added a post function to the Transaction List serializer class in views

--- finance_tracker/api/views.py
+++ finance_tracker/api/views.py
@@ -16,4 +16,9 @@ class TransactionList(APIView):
         serializer = TransactionSerializer(transactions, many=True)
         return Response(serializer.data)
     
-    
+    def post(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
