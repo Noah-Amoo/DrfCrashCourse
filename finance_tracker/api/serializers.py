2afaa2792a9159ee3fe2de655ee2f4052d475626
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-03 21:28:36 +0100

    Added a validation function for the amount field in the transaction serializer

--- finance_tracker/api/serializers.py
+++ finance_tracker/api/serializers.py
@@ -5,3 +5,10 @@ class TransactionSerializer(serializers.modelSerializer):
     class Meta:
         model = Transaction
         fields = ['id', 'amount', 'transaction_type', 'date', 'description']
+
+    # DRF follows the validate_<fieldname> to check for which field to validate, in this case, amount.
+    def validate_amount(self, value):
+        if value <=0:
+            raise serializers.ValidationError("Amount must be greater than zero")
+        
+        return value
