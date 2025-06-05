47b445a36a0c6e9ca3266ff8479716ce3f53816a
Noah Amoo <yawnoah3n@yahoo.com>
2025-06-05 18:15:31 +0100

    Obtain and Refresh jwt token paths added in urls

--- finance_tracker/api/urls.py
+++ finance_tracker/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import homeAPI, TransactionList, TransactionDetail
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', homeAPI),
     path('transactions/', TransactionList.as_view(), name='transaction-list'),
     path("transactions/<int:pk>", TransactionDetail.as_view(), name="transaction-detail"),
-]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] 
